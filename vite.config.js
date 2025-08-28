af2c975b1cc48778058684bd9836b462845bf5b9
MB <mike@vukk.la>
2025-08-28 09:19:53 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
   build: {
     target: 'es2019',
-    sourcemap: false,
-    manifest: true,
-    outDir: 'dist',
-    assetsDir: 'assets'
+    sourcemap: false
   },
   server: {
-    port: 5173,
     strictPort: true
   }
 })
